9c33974d363f9a8aea18e4c1c74a2a351176b3be
yonsweng <yonsweng@gmail.com>
2020-07-23 04:12:41 +0900

    Integrate to train.py

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ import argparse
 import torch
 from src import dqn, reinforce
 
-parser = argparse.ArgumentParser(description='train-gpu0')
+parser = argparse.ArgumentParser(description='train')
+parser.add_argument('--gpu', default=0, type=int, metavar='i', help='cuda:i')
 parser.add_argument('--dqn', default=False, action='store_true')
 parser.add_argument('--lr', default=0.001, type=float, metavar='f', help='learning rate')
 parser.add_argument('--log-interval', default=10, type=int, metavar='i', help='log interval')
@@ -10,7 +11,7 @@ parser.add_argument('--gamma', default=0.99, type=float, metavar='f', help='gamm
 parser.add_argument('--seed', default=543, type=int, metavar='i', help='seed')
 args = parser.parse_args()
 
-device = torch.device('cuda:0')
+device = torch.device(f'cuda:{args.gpu}')
 
 if args.dqn:
     dqn.train(device, args)
