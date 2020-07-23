e853d39f7ab11aac50e7c0f2e6fcffc5eec77a28
yonsweng <yonsweng@gmail.com>
2020-07-23 22:24:08 +0900

    DQN with color

--- train.py
+++ train.py
@@ -8,14 +8,13 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
-from skimage.color import rgb2gray
 from skimage.transform import resize
 from src.dqn import DQN
 
 
 def preprocess(X):
-    # Convert to grayscale and resize
-    return np.uint8(resize(rgb2gray(X), (RESIZED_SIZE, RESIZED_SIZE),
+    # Resize
+    return np.uint8(resize(X / 255, (RESIZED_SIZE, RESIZED_SIZE),
                     mode='reflect') * 255)
 
 
@@ -34,17 +33,19 @@ def train(batch):
     d_stack = []
 
     for s_r, a_r, r_r, d_r in batch:
-        s_stack.append(s_r[:HISTORY_SIZE, :, :])
+        s_stack.append(s_r[0])
         a_stack.append(a_r)
         r_stack.append(r_r)
-        s1_stack.append(s_r[1:, :, :])
+        s1_stack.append(s_r[1])
         d_stack.append(d_r)
 
     r_stack = np.array(r_stack, dtype='float32')
     d_stack = np.array(d_stack, dtype='float32')
     s_stack = np.array(s_stack, dtype='float32') / 255
+    s_stack = s_stack.transpose(0, 3, 1, 2)
     s_stack = torch.tensor(s_stack, device=device)
     s1_stack = np.array(s1_stack, dtype='float32') / 255
+    s1_stack = s1_stack.transpose(0, 3, 1, 2)
     s1_stack = torch.tensor(s1_stack, device=device)
 
     Q1 = targetDQN(s1_stack).detach().cpu().numpy().max(axis=1)
@@ -78,7 +79,6 @@ if __name__ == '__main__':
 
     # Constants
     HOUR_MIN = str(now.hour * 10000 + now.minute * 100 + now.second)
-    HISTORY_SIZE = 4
     RESIZED_SIZE = 84  # Input size of DQN
     N_ACTIONS = env.action_space.n
     START_EPS = 1.0
@@ -87,13 +87,14 @@ if __name__ == '__main__':
     MEMORY_SIZE = 50000  # Steps
     MAINDQN_UPDATE_CYCLE = 4
     TARGETDQN_UPDATE_CYCLE = 20000  # Steps
+    PLOT_CYCLE = 20
     TRAIN_START = 10000  # Steps
     BATCH_SIZE = 32
     DISCOUNT = 0.99
 
     device = torch.device(f'cuda:{args.gpu}')
 
-    mainDQN = DQN(HISTORY_SIZE, N_ACTIONS).to(device)
+    mainDQN = DQN(3, N_ACTIONS).to(device)
     targetDQN = copy.deepcopy(mainDQN)  # On device
 
     optimizer = torch.optim.Adam(mainDQN.parameters(), lr=args.lr)
@@ -111,19 +112,19 @@ if __name__ == '__main__':
         # Episode-wide variables
         episode += 1
         episode_reward = 0
-        history = np.zeros([HISTORY_SIZE+1, RESIZED_SIZE, RESIZED_SIZE],
+        history = np.zeros([2, RESIZED_SIZE, RESIZED_SIZE, 3],
                            dtype=np.uint8)
 
         state = env.reset()
         state = preprocess(state)
-        prev_state = state
+        history[0] = state
 
         while True:  # Until an episode ends
             # Step-wide variables
             step += 1
 
             # Select an action
-            X = history[:HISTORY_SIZE, :, :].astype('float32') / 255
+            X = history[0].transpose(2, 0, 1).astype('float32') / 255
             X = torch.tensor(X, device=device)
             X = X.unsqueeze(0)
             Q = mainDQN(X).tolist()[0]
@@ -134,12 +135,10 @@ if __name__ == '__main__':
 
             # Save history in replay_memory
             state = preprocess(state)
-            state = state - prev_state
-            prev_state = state
-            history[HISTORY_SIZE, :, :] = state
-            replay_memory.append((history[:, :, :].copy(),
+            history[1] = state
+            replay_memory.append((history.copy(),
                                  action, reward, done))
-            history[:HISTORY_SIZE, :, :] = history[1:, :, :]
+            history[0] = history[1]
 
             # Decrease the exploration rate
             if eps > FINAL_EPS:
@@ -160,7 +159,7 @@ if __name__ == '__main__':
 
         episode_rewards.append(episode_reward)
 
-        if episode % 10 == 0:
+        if episode % PLOT_CYCLE == 0:
             avg_reward_per_episode = sum(episode_rewards)/len(episode_rewards)
             avg_rewards.append(avg_reward_per_episode)
             episode_rewards = []
@@ -169,7 +168,7 @@ if __name__ == '__main__':
             plt.clf()
             plt.xlabel('Episode')
             plt.ylabel('Average reward per epiosde')
-            plt.plot(range(10, episode+1, 10), avg_rewards)
+            plt.plot(range(PLOT_CYCLE, episode+1, PLOT_CYCLE), avg_rewards)
             plt.savefig(f'graphs/{HOUR_MIN}.png')
 
             # Save the best model
